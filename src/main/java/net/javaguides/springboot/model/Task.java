655d19d98067f28aaf36f73f04d4463e86164aa9
ShafaetZamanShuvo <shafaetzaman937@gmail.com>
2023-07-06 12:46:57 +0600

    Added some HTML dummy pages

--- src/main/java/net/javaguides/springboot/model/Task.java
+++ src/main/java/net/javaguides/springboot/model/Task.java
@@ -16,4 +16,16 @@ public class Task {
     public Long getId() {
         return id;
     }
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
 }
